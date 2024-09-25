d27a73d24031d9b1f859c9a6f011392dc51033c7
Kevin Partin <kevin.partin@aegisaero.com>
2024-09-25 17:34:22 -0500

    Initial commit.

---
+++ test-asyncio-tkinter-1.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+
+'''
+This program demonstrates mixing asyncio and tkinter.
+'''
+
+import tkinter as tk
+from tkinter import messagebox
+import asyncio
+import threading
+import random
+
+    
+async def one_url(url: int):
+    """ One task. """
+    sec = random.randint(1, 8)
+    await asyncio.sleep(sec)
+    return 'url: {}\tsec: {}'.format(url, sec)
+
+
+async def do_urls():
+    """ Creating and starting 10 tasks. """
+    tasks = [asyncio.create_task(one_url(url)) for url in range(10)]
+    completed, pending = await asyncio.wait(tasks)
+    results = [task.result() for task in completed]
+    print('\n'.join(results))
+
+
+def _asyncio_thread(async_loop: asyncio.AbstractEventLoop):
+    async_loop.run_until_complete(do_urls())
+
+
+def do_asyncio_tasks(async_loop):
+    """ Button-Event-Handler starting the asyncio part. """
+    threading.Thread(target=_asyncio_thread, args=(async_loop,)).start()
+
+
+def do_other_tasks():
+    messagebox.showinfo(message='Tkinter is reacting.')
+
+
+def main(async_loop):
+    root = tk.Tk()
+    tk.Button(master=root, text='Asyncio Tasks', command=lambda:do_asyncio_tasks(async_loop)).pack(anchor=tk.N, fill=tk.X)
+    tk.Button(master=root, text='Other Tasks', command=do_other_tasks).pack(anchor=tk.N, fill=tk.X)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    async_loop = asyncio.new_event_loop()
+    main(async_loop)
+
---
+++ test-asyncio-tkinter-3.py
@@ -0,0 +1,89 @@
+#! /usr/bin/env python3
+
+'''
+This program demonstrates mixing asyncio and tkinter.
+'''
+
+import asyncio
+import random
+import sys
+import threading
+
+import tkinter as tk
+from tkinter import messagebox
+
+
+asyncioEvent = threading.Event()
+
+
+async def asyncioTask(url: int):
+    '''
+    An Async I/O Task.
+    '''
+    sec = random.randint(1, 8)
+    await asyncio.sleep(sec)
+    return 'url: {}\tsec: {}'.format(url, sec)
+
+
+async def asyncioTasks():
+    '''
+    Create and start 10 Async I/O Tasks.
+    '''
+    tasks = [asyncio.create_task(asyncioTask(n)) for n in range(10)]
+    completed, pending = await asyncio.wait(tasks)
+    results = [task.result() for task in completed]
+    print('\n'.join(results))
+    asyncioEvent.set()
+
+
+def asyncioMain():
+    '''
+    Starts the Ansyc I/O event loop.
+    '''
+    asyncio.run(asyncioTasks())
+
+
+def asyncioEventMonitor():
+    asyncioEvent.wait()
+    messagebox.showinfo(message=f'Async I/O Tasks complete.')
+
+
+def guiTasks(n: int):
+    messagebox.showinfo(message=f'Performing Task {n}')
+
+
+def guiMain():
+    threading.Thread(target=asyncioEventMonitor).start()
+    root = tk.Tk()
+    body = tk.Frame(master=root)
+    body.pack(anchor=tk.CENTER, fill=tk.BOTH, padx=10, pady=10)
+    tk.Button(master=body, text='Task 1', command=lambda: guiTasks(1)).pack(anchor=tk.N, fill=tk.X, pady=(0, 5))
+    tk.Button(master=body, text='Task 2', command=lambda: guiTasks(2)).pack(anchor=tk.N, fill=tk.X, pady=(0, 5))
+    tk.Button(master=body, text='Task 3', command=lambda: guiTasks(3)).pack(anchor=tk.N, fill=tk.X, pady=(0, 5))
+    tk.Button(master=body, text='Exit', command=root.destroy).pack(anchor=tk.N, fill=tk.X, pady=(5, 0))
+    root.mainloop()
+
+
+if __name__ == '__main__':
+
+    guit = threading.Thread(
+        target = guiMain,
+        name = 'GUI',
+    )
+
+    aiot = threading.Thread(
+        target= asyncioMain,
+        name = 'Async I/O',
+    )
+
+    guit.start()
+
+    aiot.start()
+
+    aiot.join()
+    print('Async I/O complete')
+
+    guit.join()
+    print('Done')
+
+    sys.exit(0)
